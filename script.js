e1dab9629a76d41e01e1c70570dd5f0cdf3d24a0
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-10 21:26:10 +0200

    created functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var dictionary = [""];
+var i = 1;
 
+function addWord() {
+  var word = document.getElementById("wordinput").value;
+  dictionary.push(word);
+  document.getElementById("wordInput") = "";
+}
+
+function searchWord() {}
+
+for (let j = 0; j < dictionary.length; i++) {
+  console.log(dictionary[j]);
+}
